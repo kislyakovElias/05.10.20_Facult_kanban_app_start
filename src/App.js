303769227f591cb3debb15946b406da9b8053bfa
superfoxx <super_fox@mail.ru>
2020-10-06 00:18:27 -0700

    updated scr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Column from './Column'
+import 'bootstrap/dist/css/bootstrap.css'
+
+  const taskArray = [
+    {id:Math.random(), name: 'Drink', status: 'todo'},
+    {id:Math.random(), name: 'Eat', status: 'todo'},
+    {id:Math.random(), name: 'Sleep', status: 'todo'},
+    {id:Math.random(), name: 'Workout', status: 'todo'},
+    {id:Math.random(), name: 'Chill', status: 'todo'},
+    {id:Math.random(), name: 'FE5', status: 'progress'},
+    {id:Math.random(), name: 'Code', status: 'review'},
+    {id:Math.random(), name: 'More code', status: 'review'},
+    {id:Math.random(), name: 'Learn more', status: 'review'},
+    {id:Math.random(), name: 'Even more', status: 'review'},
+    {id:Math.random(), name: 'Sit and look in monitor', status: 'done'},
+  ];
+
+  const columnArray = [
+    {id: Math.random(), title: "To do", status: 'todo'},
+    {id: Math.random(), title: "Progress", status: 'progress'},
+    {id: Math.random(), title: "Review", status: 'review'},
+    {id: Math.random(), title: "Done", status: 'done'},
+  ];
+
+  const statuses = ["todo", "progress", "review", "done"];
 
 function App() {
+
+  const [task, setTask] = useState(taskArray);
+
+  const changeTaskStatus = (taskId, direction) => {
+    const newTasks = task.map(el => {
+      if(el.id === taskId){
+        if(direction === "right") el.status = statuses[statuses.indexOf(el.status) + 1];
+        if(direction === "left") el.status = statuses[statuses.indexOf(el.status) - 1];
+      }
+return el
+    })
+setTask(newTasks)
+  }
+
+  console.log('tasks', task)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <div className='row'>{columnArray.map((el, index) => <Column column={el}
+                                                        tasks={task}
+                                                        changeTaskStatus={changeTaskStatus}
+                                                        index={index}
+                                                        statuses={statuses}
+
+
+    />)}
+    </div>
+
     </div>
   );
 }
